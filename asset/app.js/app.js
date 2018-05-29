d95c0c6679f0fc6af7ad145e39a9fdde223a7052
AbrahamTesla <manlangitv@gmail.com>
2018-05-28 22:14:13 -0400

    made changes to asset and index

--- asset/app.js/app.js
+++ asset/app.js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function){
+$(document).ready(function(){
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCLXx9HaKt44bWCGyOT4qEA0vEm5-LFA4M",
@@ -23,15 +23,58 @@ $("#train").on("click", function(event){
     
     var result ="";
     
+    
+    
     database.ref().push({
         trainName: trainName,
         destination: destination,
         firstTrain: firstTrain,
         frequency: frequency
     });
+$("#trainName").val(""); 
+  $("#destination").val("");
+    $("#firstTrain").val("");
+      $("#frequency").val("");
+});
+
+database.ref().on('child_added',function(childSnaphshot){
+    var name = childSnaphshot.val().trainName;
+    var dest = childSnaphshot.val().destination;
+    var fTrain = childSnaphshot.val().firstTrain;
+    var freq = childSnaphshot.val().frequency;
     
-}
+//    console.log("Name: " + name);
+//	console.log("Destination: " + dest);
+//	console.log("Time: " + time);
+//	console.log("Frequency: " + freq);
+    var freq = parseInt(freq);
+    
+    var currentTime= moment();
+    
+    var convertDate = moment(fTrain,'HH:mm').subtract(1,'years');
+    console.log("DATE CONVERTED: " + convertDate);
+    var trainTime = moment(convertDate).format('HH:mm');
+    console.log("TRAIN TIME : " + trainTime);
 
-
-
+    var timeConvert = moment(trainTime,'HH:mm').subtract(1,'years');
+    
+    var difference = moment().diff(timeConvert,'minutes');
+    
+    var timeRemain = difference % freq;
+    
+    var minsAway = freq - timeRemain;
+    
+    var nextTrain = moment().add(minsAway,'minutes');
+    
+    
+    $("#result").append(
+         "<tr><td id='nameResult'>"+name +
+         "</td><td id='destResult'>"+dest +
+         "</td><td id='freqResult'>"+freq+
+         "</td><td id='nextArrival'>"+moment(nextTrain).format('HH:mm')+
+        "</td><td id='timeAway'>"+minsAway + ' mins til Arrival'+"</td></tr>");
+    
+    
+});
+    
 });
